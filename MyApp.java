6bc7f15768c9519e3f78d1e37edebe64be54171c
kishorebraj8 <80564801+kishorebraj8@users.noreply.github.com>
2021-04-06 11:10:03 +0530

    Add files via upload

---
+++ MyApp.java
@@ -0,0 +1,13 @@
+
+public class MyApp {
+	public static void main(String[] args) {
+		
+		int number=34;
+		while(number<56) {
+			System.out.println("okay"+","+number);
+			number=number+2;
+		}
+
+	}
+
+}
